e817261fbd7f9aa0d7e1f0ac368c380ef9189670
David Liberal <davidliberal0@gmail.com>
2024-11-28 12:52:12 -0500

    add main functional component

--- App.js
+++ App.js
@@ -1,2 +1,8 @@
 import React, { useState } from 'react';
 import {StyleSheet, Text, View, TextInput, Button, FlatList, TouchableOpacity } from 'react-native';
+
+// main functional component 
+export default function App() {
+  const [task, setTask] = useState('');
+  const [tasks, setTasks] = useState([]); 
+} 
